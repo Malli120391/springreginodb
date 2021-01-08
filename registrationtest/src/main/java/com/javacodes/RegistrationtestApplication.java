c51a229a330480560846fe5e8f8cf904e74cd13d
Malleswara Rao <ch.malli1203@gmail.com>
2021-01-08 18:43:08 +0530

    spring boot small regi form no database

---
+++ registrationtest/src/main/java/com/javacodes/RegistrationtestApplication.java
@@ -0,0 +1,13 @@
+package com.javacodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationtestApplication.class, args);
+	}
+
+}
